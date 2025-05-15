4ab04eebcd92f8e2610c17828550dded5f63fec6
Alexei <streiker.saik@gmail.com>
2025-05-15 10:26:36 +0300

    update config.py + test

--- src/config.py
+++ src/config.py
@@ -2,7 +2,7 @@ from configparser import ConfigParser
 from typing import Any, Dict
 
 
-def config(file_name="database.ini", section="postgresql") -> Dict[str, Any]:
+def config(file_name: str = "database.ini", section: str = "postgresql") -> Dict[str, Any]:
     """
     Парсинг параметров из файла конфигурации базы данных
     :param file_name: Наименование файла конфигурации (по умолчанию "database.ini")
@@ -17,5 +17,5 @@ def config(file_name="database.ini", section="postgresql") -> Dict[str, Any]:
         for param in params:
             db[param[0]] = param[1]
     else:
-        raise Exception('Section {0} is not found in the {1} file.'.format(section, file_name))
+        raise Exception("Section {0} is not found in the {1} file.".format(section, file_name))
     return db
